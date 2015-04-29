e3fa33a549f2bea28c04aba1dfa3d7b3fb72565d
Amit Patel <amit@mramitpatel.com>
2015-04-29 12:29:07 -0500

    revised for NA

--- js/svganimations.js
+++ js/svganimations.js
@@ -137,7 +137,7 @@
 	}
 	
 	function init() {
-		var svgs = Array.prototype.slice.call( document.querySelectorAll( '#main svg' ) ),
+		var svgs = Array.prototype.slice.call( document.querySelectorAll( 'svg' ) ),
 			svgArr = new Array(),
 			didScroll = false,
 			resizeTimeout;
